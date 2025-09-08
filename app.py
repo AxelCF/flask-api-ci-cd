50660b28fddd875fe0120372220b0f51cd786d6d
AxelCF <axelmaurice.1997@gmail.com>
2025-09-08 12:47:58 +0200

    Utilise sqlite en mémoire

--- app.py
+++ app.py
@@ -2,16 +2,22 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/mydb")
-db = SQLAlchemy(app)
 
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URL", "postgresql://user:password@db:5432/mydb"
+)
+
+db = SQLAlchemy(app)
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     completed = db.Column(db.Boolean, default=False)
 
+if os.getenv("CI") == "true":
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
 
 with app.app_context():
     db.create_all()
